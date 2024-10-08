c7b71f2c067e6ddc81b3989448d217d039dfb039
Daniel Cardona <danielcardona288459@correo.itm.edu.co>
2024-10-08 04:28:26 -0500

    Ajuste con migracion

---
+++ ArrendamientoSoftware.Web/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ArrendamientoSoftware.Web.Data.Entities;
+using static System.Collections.Specialized.BitVector32;
+using System.Reflection.Metadata;
+using ArrendamientoSoftware.Web.Data.Entities.PrivateBlog.Web.Data.Entities;
+
+namespace ArrendamientoSoftware.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Propiedad> Properties { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
