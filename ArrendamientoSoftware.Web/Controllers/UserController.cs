ba227efbfb552196ff4e04f4823506f18196fdd9
Daniel Cardona <danielcardona288459@correo.itm.edu.co>
2024-10-08 22:59:01 -0500

    Otro cambio a ver si funciona

---
+++ ArrendamientoSoftware.Web/Controllers/UserController.cs
@@ -0,0 +1,114 @@
+﻿using ArrendamientoSoftware.Web.Data;
+using ArrendamientoSoftware.Web.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArrendamientoSoftware.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<User> users = await _context.Users.ToListAsync();
+            return View("Users",users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(user);
+                }
+
+                await _context.Users.AddAsync(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "No se pudo crear el usuario. Inténtelo de nuevo.");
+                return View(user);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit([FromRoute] int id)
+        {
+            try
+            {
+                User? user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+                return View(user);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(User user)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(user);
+                }
+
+                _context.Users.Update(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "No se pudo actualizar el usuario. Inténtelo de nuevo.");
+                return View(user);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                User? user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
---
+++ ArrendamientoSoftware.Web/Services/IUsersService.cs
@@ -0,0 +1,43 @@
+﻿
+using ArrendamientoSoftware.Web.Core;
+using ArrendamientoSoftware.Web.Data;
+using ArrendamientoSoftware.Web.Data.Entities;
+
+namespace ArrendamientoSoftware.Web.Services
+{
+
+    public interface IUsersService
+    {
+        public Task<Response<List<User>>> GetListAsync();
+
+    }
+
+    public class UsersService : IUsersService
+    {
+        private readonly DataContext _context;
+
+        public UsersService(DataContext context)
+        {
+            _context = context;
+
+        }
+    }
+
+    public async Task<Response<List<User>> GetListAsync()
+    {
+        try
+        {
+
+        }
+        catch(Exception ex) {
+        {
+            
+        }
+    }
+
+        
+
+
+
+
+}
